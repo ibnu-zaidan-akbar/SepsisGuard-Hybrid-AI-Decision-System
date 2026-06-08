0dd3f9006d6bb86a435c6e132b66714f086da675
ibnu-zaidan-akbar <akbaribnu031@gmail.com>
2026-06-08 17:08:24 +0700

    feat: add easy to read dataset files

--- sespsis-ai/src/app/page.tsx
+++ sespsis-ai/src/app/page.tsx
@@ -193,6 +193,15 @@ export default function SepsisDashboard() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
       });
+
+       // klo udh deploy di railway
+      // const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+    
+      // const response = await fetch(`${API_URL}/predict`, {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify(payload),
+      // });
       const data = await response.json();
       setHasilPrediksi(data);
     } catch (error) {
